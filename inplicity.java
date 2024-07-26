978e06726882e6a1c69ce50ee1c53ce527754d68
maitri217 <maitri21072004@gmail.com>
2024-07-26 13:25:19 +0530

    Add files via upload

---
+++ inplicity.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class inplicity {
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Integer Value of q: ");
+        int q = input.nextInt();
+        int r=q;
+        System.out.println("int value:"+r);
+        long s=r;
+        System.out.println("long value:"+s);
+        float t=s;
+        System.out.println("float value:"+t);
+    }
+}

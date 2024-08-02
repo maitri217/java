f9a01eaafc07b593b96b2714c2830e139fe69b03
maitri217 <maitri21072004@gmail.com>
2024-08-02 12:52:14 +0530

    Add files via upload

--- instance.java
+++ instance.java
@@ -1,31 +1,25 @@
-public class instance {
-    // Instance variables
-    // These variables are specific to each instance of the Car class.
-    String brand;
-    int year;
-    boolean isElectric;
+public class static {
+    // Static variable
+    // This variable is shared among all instances of the Counter class.
+    static int count = 0;
 
-    // Constructor to initialize the instance variables
-    public Car(String brand, int year, boolean isElectric) {
-        this.brand = brand;
-        this.year = year;
-        this.isElectric = isElectric;
+    // Constructor to increment the static variable 'count'
+    public Counter() {
+        count++;
     }
 
-    // Method to display car information
-    public void displayInfo() {
-        System.out.println("Car Brand: " + brand);
-        System.out.println("Manufacture Year: " + year);
-        System.out.println("Is Electric: " + isElectric);
+    // Method to display the value of the static variable 'count'
+    public static void displayCount() {
+        System.out.println("Number of instances created: " + count);
     }
 
     public static void main(String[] args) {
-        // Creating objects (instances) of the Car class
-        Car car1 = new Car("Tesla", 2020, true);
-        Car car2 = new Car("Toyota", 2015, false);
+        // Creating instances of the Counter class
+        Counter c1 = new Counter();
+        Counter c2 = new Counter();
+        Counter c3 = new Counter();
 
-        // Each object has its own set of instance variables
-        car1.displayInfo(); // Displays information about car1
-        car2.displayInfo(); // Displays information about car2
+        // All instances share the same static variable 'count'
+        Counter.displayCount(); // Output will be: Number of instances created: 3
     }
 }
---
+++ staticc.java
@@ -0,0 +1,31 @@
+public class staticc {
+    // Instance variables
+    // These variables are specific to each instance of the Car class.
+    String brand;
+    int year;
+    boolean isElectric;
+
+    // Constructor to initialize the instance variables
+    public Car(String brand, int year, boolean isElectric) {
+        this.brand = brand;
+        this.year = year;
+        this.isElectric = isElectric;
+    }
+
+    // Method to display car information
+    public void displayInfo() {
+        System.out.println("Car Brand: " + brand);
+        System.out.println("Manufacture Year: " + year);
+        System.out.println("Is Electric: " + isElectric);
+    }
+
+    public static void main(String[] args) {
+        // Creating objects (instances) of the Car class
+        Car car1 = new Car("Tesla", 2020, true);
+        Car car2 = new Car("Toyota", 2015, false);
+
+        // Each object has its own set of instance variables
+        car1.displayInfo(); // Displays information about car1
+        car2.displayInfo(); // Displays information about car2
+    }
+}

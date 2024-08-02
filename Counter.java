64817fd36d647524cf049be1bae4193dde4f3794
maitri217 <maitri21072004@gmail.com>
2024-08-02 12:53:32 +0530

    Add files via upload

---
+++ Counter.java
@@ -0,0 +1,25 @@
+public class Counter {
+    // Static variable
+    // This variable is shared among all instances of the Counter class.
+    static int count = 0;
+
+    // Constructor to increment the static variable 'count'
+    public Counter() {
+        count++;
+    }
+
+    // Method to display the value of the static variable 'count'
+    public static void displayCount() {
+        System.out.println("Number of instances created: " + count);
+    }
+
+    public static void main(String[] args) {
+        // Creating instances of the Counter class
+        Counter c1 = new Counter();
+        Counter c2 = new Counter();
+        Counter c3 = new Counter();
+
+        // All instances share the same static variable 'count'
+        Counter.displayCount(); // Output will be: Number of instances created: 3
+    }
+}
